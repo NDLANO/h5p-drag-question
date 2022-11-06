3749a867257bb0b4151401f675349c169d41c6ab
Oliver Tacke <o.tacke@posteo.de>
2022-11-06 12:44:56 +0100

    Update build chain

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,74 @@
-var path = require('path');
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserPlugin = require('terser-webpack-plugin');
+const libraryName = process.env.npm_package_name;
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = (nodeEnv === 'production');
 
 module.exports = {
-  entry: './src/drag-question.js',
+  mode: nodeEnv,
+  optimization: {
+    minimize: isProd,
+    minimizer: [
+      new TerserPlugin({
+        terserOptions: {
+          compress:{
+            drop_console: true,
+          }
+        }
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: `css/${libraryName}.css`
+    })
+  ],
+  entry: {
+    dist: './src/drag-question.js'
+  },
   output: {
-    filename: 'h5p-drag-question.js',
+    filename: `${libraryName}.js`,
     path: path.resolve(__dirname, '.')
   },
+  target: ['web'],
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
+        exclude: /node_modules/,
         loader: 'babel-loader'
+      },
+      {
+        test: /\.(s[ac]ss|css)$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: ''
+            }
+          },
+          { loader: "css-loader" },
+          {
+            loader: "sass-loader"
+          }
+        ]
+      },
+      {
+        test: /\.svg|\.jpg|\.png$/,
+        include: path.join(__dirname, 'src/images'),
+        type: 'asset/resource'
+      },
+      {
+        test: /\.woff$/,
+        include: path.join(__dirname, 'src/fonts'),
+        type: 'asset/resource'
       }
     ]
-  }
+  },
+  stats: {
+    colors: true
+  },
+  devtool: (isProd) ? undefined : 'inline-source-map'
+
 };
